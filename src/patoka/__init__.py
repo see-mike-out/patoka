8bd7b920072b61be330ca8f53515ee21aea7d182
Hyeok Kim <hyeokkim@Hyeoks-MacBook-Pro-3.local>
2025-02-04 10:20:24 -0800

    import fix

--- src/patoka/__init__.py
+++ src/patoka/__init__.py
@@ -2,6 +2,6 @@
 from patoka.widgets import getCircuitViewer, getCircuitWriter, getResultViewer, getMachineViewer
 from patoka.widget_classes import CircuitWriter, CircuitViewer, MachineViewer, ResultViewer
 from patoka.job_output_data import JobOutputData
-from patoka.process_job_data import processJobData
+from patoka.process_job_data import processJobData, processSimJobData
 from patoka.retrieve_job_data import retrieveJobData
 from patoka.transpile_params import TranspileParam
